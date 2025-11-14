e67054fdaf455e4fcc5ac5d92cf4d7d2f8aa4150
Vasyl Veselov <vasyl.veselov@monobank.ua>
2025-11-14 14:37:46 +0200

    init

---
+++ src/test/java/com/ntudp/vasyl/veselov/master/diploma/DiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntudp.vasyl.veselov.master.diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
