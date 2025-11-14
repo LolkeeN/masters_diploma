9c0a49f96a648827652a65aad9f48feadbe376f3
Vasyl Veselov <vasyl.veselov@monobank.ua>
2025-11-14 16:10:27 +0200

    java 25

--- postgres/src/test/java/repository/UserRepositoryTest.java
+++ postgres/src/test/java/repository/UserRepositoryTest.java
@@ -1,9 +1,14 @@
 package repository;
 
 import com.ntudp.vasyl.veselov.master.BaseTestParent;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRepositoryTest extends BaseTestParent {
 
+    @Test
+    void test() {
+
+    }
 }
