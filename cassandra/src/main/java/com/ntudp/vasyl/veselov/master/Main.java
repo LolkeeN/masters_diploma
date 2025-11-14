2becbb42e8e8d3af390a07c5934980d05fa2e490
Vasyl Veselov <vasyl.veselov@monobank.ua>
2025-11-15 01:26:18 +0200

    added @SpringBootApplication for every module

--- cassandra/src/main/java/com/ntudp/vasyl/veselov/master/Main.java
+++ cassandra/src/main/java/com/ntudp/vasyl/veselov/master/Main.java
@@ -1,7 +1,10 @@
 package com.ntudp.vasyl.veselov.master;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
