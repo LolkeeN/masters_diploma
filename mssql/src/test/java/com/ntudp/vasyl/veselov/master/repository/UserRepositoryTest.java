254f3890380ce8929870503b0b7ecd8ea1cb67ec
Vasyl Veselov <vasyl.veselov@monobank.ua>
2025-11-20 23:12:09 +0200

    mssql first statistics

--- mssql/src/test/java/com/ntudp/vasyl/veselov/master/repository/UserRepositoryTest.java
+++ mssql/src/test/java/com/ntudp/vasyl/veselov/master/repository/UserRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.ntudp.vasyl.veselov.master.repository;
 
 import com.ntudp.vasyl.veselov.master.dto.SqlUser;
+import com.ntudp.vasyl.veselov.master.util.CsvUtil;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import lombok.extern.slf4j.Slf4j;
@@ -56,6 +58,12 @@ class UserRepositoryTest {
         Random rand = new Random();
         List<SqlUser> users = new ArrayList<>();
         AtomicInteger atomicInteger = new AtomicInteger();
+        List<String[]> dataLines = new ArrayList<>();
+        dataLines.add(new String[] {
+                "Operation", "Time (ms)"
+        });
+
+        long start = System.currentTimeMillis();
         Executors.newFixedThreadPool(15).execute(() -> {
             while (true) {
                 int i = atomicInteger.incrementAndGet();
@@ -82,9 +90,61 @@ class UserRepositoryTest {
             monitor.wait();
         }
 
-        log.info("All users created. Total: {}", userRepository.findAll().size());
-        log.info("Users with friends:{}", userRepository.findAll().stream()
-                .filter(user -> !user.getFriends().isEmpty())
-                .count());
+        dataLines.add(new String[] {
+                "Generate starting users", String.valueOf(System.currentTimeMillis() - start)
+        });
+
+
+        start = System.currentTimeMillis();
+        userRepository.findAll()
+                .stream()
+                .filter(u -> u.getFriends().size() > 1)
+                .count();
+
+        dataLines.add(new String[] {
+                "Find all and interact with many to many relation", String.valueOf(System.currentTimeMillis() - start)
+        });
+
+        start = System.currentTimeMillis();
+        userRepository.findById(users.get(rand.nextInt(users.size() - 1)).getId());
+        dataLines.add(new String[] {
+                "Find by id", String.valueOf(System.currentTimeMillis() - start)
+        });
+
+        start = System.currentTimeMillis();
+        SqlUser randomUser = users.get(rand.nextInt(users.size() - 1));
+        userRepository.deleteUserWithFriendships(randomUser.getId());
+        dataLines.add(new String[] {
+                "Delete by id with all friendships", String.valueOf(System.currentTimeMillis() - start)
+        });
+        users.remove(randomUser);
+
+        start = System.currentTimeMillis();
+        userRepository.count();
+        dataLines.add(new String[] {
+                "Count all", String.valueOf(System.currentTimeMillis() - start)
+        });
+
+        start = System.currentTimeMillis();
+        randomUser = users.get(rand.nextInt(users.size() - 1));
+        randomUser.setEmail(UUID.randomUUID().toString());
+        userRepository.save(randomUser);
+        dataLines.add(new String[] {
+                "Update user", String.valueOf(System.currentTimeMillis() - start)
+        });
+//
+//        start = System.currentTimeMillis();
+//        userRepository.findByEmail(randomUser.getEmail());
+//        dataLines.add(new String[] {
+//                "Find by email", String.valueOf(System.currentTimeMillis() - start)
+//        });
+//
+//        start = System.currentTimeMillis();
+//        userRepository.findByFriendsId(randomUser.getId());
+//        dataLines.add(new String[] {
+//                "Find by friends id", String.valueOf(System.currentTimeMillis() - start)
+//        });
+
+        CsvUtil.generateFile("mssql_statistics", dataLines);
     }
 }
